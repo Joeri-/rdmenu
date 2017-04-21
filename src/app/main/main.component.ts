82c295dfca130e84bf57c0ff73ac8a20a4bc541b
Joeri- <joerijackers@gmail.com>
2017-04-21 12:05:11 +0200

    semantic UI and some logic implemented

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MealsService } from '../meals.service';
+import { Meal, MealsService } from '../meals.service';
 
 @Component({
   selector: 'app-main',
@@ -11,15 +11,55 @@ export class MainComponent implements OnInit {
   mealNames: string[];
   ingrAldiList : string[];
   ingrCarrList : string[];
+  menu: Meal[];
 
 
   constructor( private meals: MealsService) {
-    this.mealNames = meals.meals.map(x => x.name);
-    this.ingrAldiList = meals.meals.map(x => x.ingrAldi.join()).join().split(',');
-    this.ingrCarrList = meals.meals.map(x => x.ingrCarr.join()).join().split(',');
+    this.menu = [];
+    this.ingrAldiList = [];
+    this.ingrCarrList = [];
    }
 
   ngOnInit() {
+  }
+
+  addToMenu(meal: Meal) {
+    if (this.menu.indexOf(meal) < 0){
+      this.menu.push(meal);
+      this.makeAldiList();
+      this.makeCarrList();
+    }
+  }
+
+  removeFromMenu(meal: Meal) {
+    let index = this.menu.indexOf(meal);
+    if (index >= 0) {
+      this.menu.splice(index, 1);
+      this.makeAldiList();
+      this.makeCarrList();
+    }
+  }
+
+  makeAldiList() {
+    this.ingrAldiList = [];
+    for(let meal of this.menu){
+      for(let ingr of meal.ingrAldi) {
+        if (this.ingrAldiList.indexOf(ingr) < 0){
+          this.ingrAldiList.push(ingr);
+        }
+      }
+    }
+  }
+
+  makeCarrList() {
+    this.ingrCarrList = [];
+    for(let meal of this.menu){
+      for(let ingr of meal.ingrCarr) {
+        if (this.ingrCarrList.indexOf(ingr) < 0){
+          this.ingrCarrList.push(ingr);
+        }
+      }
+    }
   }
 
 }
